48119e3df7a818bfe46447b4bb58fca9fa95cac1
jtlacci <johntsterlacci@gmail.com>
2018-03-09 18:26:50 -0500

    added buy function

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,9 +1,13 @@
 pragma solidity ^0.4.18;
 
-contract Lottery {
+contract Lottery {SimpleStorage
 
   uint ticketPrice;
   uint ticketCount;
+
+  mapping(address => uint) userTickets;
+
+  bytes[] tickets;
 
   function activate(uint _price, uint _count){
     ticketPrice = _price;
@@ -15,6 +19,15 @@ contract Lottery {
   }
   function getCount() returns(uint count){
     return ticketCount;
+  }
+
+  function buy(){
+    address user = msg.sender
+    require(msg.value == ticketPrice);
+    // adds address to ticket array
+    tickets.push(user);
+    //increment user's tickets
+    userTickets[user]+= 1;
   }
 
 
