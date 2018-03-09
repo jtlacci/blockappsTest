8d4bed29d8b993abc9fa0c242d5498bbb7065bb9
jtlacci <johntsterlacci@gmail.com>
2018-03-09 18:19:38 -0500

    buy ticket test

--- test/Lottery.js
+++ test/Lottery.js
@@ -1,16 +1,23 @@
 var Lottery = artifacts.require("./Lottery.sol");
 
 contract('Lottery', function(accounts) {
+  var instance = await Lottery.deployed()
+  var ticketPrice = 1
+  var ticketCount = 20
 
   it("creator can set ticket price and count", async function() {
-    var instance = await Lottery.deployed()
-    var ticketPrice = 1
-    var ticketCount = 20
     await instance.activate(web3.toWei(ticketPrice), ticketCount)
     var cTicketPrice = await instance.getPrice.call()
     var cTicketCount = await instance.getCount.call()
     assert.equal(ticketPrice,web3.fromWei(cTicketPrice))
     assert.equal(ticketCount,cTicketCount)
+  });
+
+  it("can buy tickets", async function() {
+
+    await instance.buy({value:web3.toWei(ticketPrice)})
+    var myTickets = await instance.myTickets.call()
+    assert.equal(1,myTickets)
   });
 
 });
